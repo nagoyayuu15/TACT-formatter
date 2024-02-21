24a664a1ea6132c9e7876a0f6a0e05ce273f6ac6
suke-0612 <ousuke612@gmail.com>
2024-02-22 02:16:04 +0900

    HTMLの編集をDOM　APIを利用する形に変更

--- script.js
+++ script.js
@@ -7,30 +7,38 @@ const changeTable = () => {
     const topnav = document.getElementById("topnav");
     const links = topnav.getElementsByClassName('link-container');
     const siteLinkButtons = topnav.getElementsByClassName('Mrphs-sitesNav__menuitem ');
-    // buttonに時間割データを入れていく
-    let buttonList = new Array(NUM_OF_DAYS).fill("").map(() => new Array(NUM_OF_PERIODS).fill(""));
-    let noTimeDataSitesHTML = ""
-    for (let i = 0; i < links.length; i++) {
-            const siteName = links.item(i).title;
-            const lectureData = makeLectureData(siteName);
-            const tagBtnName = lectureData.title;
-            const tag = "<a href=" + links.item(i).href + ">" + "<span>" + tagBtnName + "</span>" + "</a>"
-            if(lectureData.time!=null){
-                buttonList[lectureData.time[0][0]][lectureData.time[0][1]] = tag;
-                // ２時間目もあったら
-                if(lectureData.time[1]!=null){
-                    buttonList[lectureData.time[1][0]][lectureData.time[1][1]] = tag;
-                }
-            }else{
-                noTimeDataSitesHTML += siteLinkButtons.item(i).outerHTML;
+    const siteLinkButtonsList = Array.from(siteLinkButtons);
+    // timeTableに時間割データを入れていく
+    let timeTable = new Array(NUM_OF_DAYS).fill(null).map(() => new Array(NUM_OF_PERIODS).fill(null));
+    // サイトボタン削除の関係で逆順で処理
+    for (let i = links.length-1; i >= 0; i--) {
+        const siteName = links.item(i).title;
+        const lectureData = makeLectureData(siteName);
+        const tagBtnName = lectureData.title;
+        // 表に入れるボタンの作成
+        const linkElement = document.createElement('a');
+        linkElement.href = links.item(i).href;
+        const spanElement = document.createElement('span');
+        spanElement.textContent = tagBtnName;
+        linkElement.appendChild(spanElement);
+        if(lectureData.time!=null){
+            timeTable[lectureData.time[0][0]][lectureData.time[0][1]] = linkElement;
+            // ２時間目もあったら
+            if(lectureData.time[1]!=null){
+                timeTable[lectureData.time[1][0]][lectureData.time[1][1]] = linkElement;
             }
+            siteLinkButtons.item(i).remove();
         }
-        // HTMLの書き換え
-        topnav.innerHTML = makeTable(buttonList) + noTimeDataSitesHTML;
-        // cssの書き換え
-        const table = document.getElementById("timeTable");
-        applyCss(table);
-    };
+        
+    }
+    
+    // HTMLの書き換え
+    topnav.insertBefore(makeTableHTML(timeTable),topnav.firstChild);
+    // cssの書き換え
+    const table = document.getElementById("timeTable");
+    applyCss(table);
+    
+};
 
 // サイト名から曜日・時限、授業名を取得
 const makeLectureData = (siteName) => {
@@ -130,35 +138,46 @@ const makeLectureData = (siteName) => {
     return {time: time, title: title};
 };
 
-// 返す時間割表の作成
-const makeTable = (buttonList) => {
-    const headTable = (NUM_OF_DAYS === 5) ? "<div></div><div>月曜日</div><div>火曜日</div><div>水曜日</div><div>木曜日</div><div>金曜日</div>" : "<div></div><div>月曜日</div><div>火曜日</div><div>水曜日</div><div>木曜日</div><div>金曜日</div><div>土曜日</div>";
-    let timeTable="";
+// 返す時間割表HTMLの作成
+const makeTableHTML = (timeTable) => {
+    const table = document.createElement('li');
+    table.id = "timeTable";
     for(let i=0;i<NUM_OF_PERIODS;i++){
-        timeTable += "<div>" + (i+1) + "限</div>";
+        const li = document.createElement('li');
+        li.textContent = (i+1) + "限";
+        table.appendChild(li);
         for (let j=0;j<NUM_OF_DAYS;j++){
-            
-            timeTable += "<div>" + buttonList[j][i] + "</div>";
+            const li = document.createElement('li');
+            if(timeTable[j][i]){
+                li.appendChild(timeTable[j][i]);
+            }
+            table.appendChild(li);
         }
     }
-    return "<div id=\"timeTable\">" + headTable + timeTable + "</div>";
+return table;
 };
 
 // cssの適用
 const applyCss = (table) =>{
+    // table.className = "Mrphs-sitesNav__menuitem";
+    
     table.style.display = "grid";
     table.style.width = "100%";
     table.style.gridTemplateColumns = "30px repeat("+NUM_OF_DAYS+", 1fr)";
     table.style.marginBottom = "10px";
+    table.style.webkitAlignItems = "stretch";
+    table.style.backgroundColor = "white";
 
-    const links = table.querySelectorAll('div');
+    const links = table.querySelectorAll('li');
     links.forEach(link => {
+        // link.className = "Mrphs-sitesNav__menuitem";
         link.style.webkitAlignItems = "center";
         link.style.alignItems = "center";
         link.style.textAlign = "center";
         link.style.border = "1px solid #ddd";
     });
     table.querySelectorAll('a').forEach(link => {
+        // link.className = "link-container";
         
         link.style.textDecoration = "none";
         link.style.color = "#404040";
